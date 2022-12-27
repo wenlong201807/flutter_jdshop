0d39946380a1540bb378366ccce80c16671102da
dragon <1573511441@qq.com>
2022-12-27 16:53:47 +0800

    feat: 1、商品列表页 - 下拉加载更多 2、加载状态封装 3、获取接口数据

--- lib/pages/ProductList.dart
+++ lib/pages/ProductList.dart
@@ -1,97 +1,193 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import '../services/ScreenAdapter.dart';
-// import '../config/Config.dart';
-// import 'package:dio/dio.dart';
+import '../config/Config.dart';
+import 'package:dio/dio.dart';
+import '../model/ProductModel.dart';
+import '../widget/LoadingWidget.dart';
 
 class ProductListPage extends StatefulWidget {
   Map arguments;
+  //required this.arguments表示arguments必须传入值
   ProductListPage({Key? key, required this.arguments}) : super(key: key);
 
-  @override
   _ProductListPageState createState() => _ProductListPageState();
 }
 
 class _ProductListPageState extends State<ProductListPage> {
+  //Scaffold key
   final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+
+  //用于上拉分页
+  ScrollController _scrollController = ScrollController(); //listview 的控制器
+
+  //分页
+  int _page = 1;
+
+  //每页有多少条数据
+  int _pageSize = 8;
+
+  //数据
+  List _productList = [];
+  /*
+  排序:价格升序 sort=price_1 价格降序 sort=price_-1  销量升序 sort=salecount_1 销量降序 sort=salecount_-1
+  */
+  String _sort = "";
+
+  //解决重复请求的问题
+  bool flag = true;
+
+  //是否有数据
+  bool _hasMore = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _getProductListData();
+
+    //监听滚动条滚动事件
+    _scrollController.addListener(() {
+      //_scrollController.position.pixels //获取滚动条滚动的高度
+      //_scrollController.position.maxScrollExtent  //获取页面高度
+      if (_scrollController.position.pixels >
+          _scrollController.position.maxScrollExtent - 20) {
+        if (this.flag && this._hasMore) {
+          _getProductListData();
+        }
+      }
+    });
+  }
+
+  //获取商品列表的数据
+  _getProductListData() async {
+    setState(() {
+      this.flag = false;
+    });
+
+    var api =
+        '${Config.domain}api/plist?cid=${widget.arguments["cid"]}&page=${this._page}&sort=${this._sort}&pageSize=${this._pageSize}';
+
+    print(api);
+    var result = await Dio().get(api);
+
+    var productList = new ProductModel.fromJson(result.data);
+
+    print(productList.result.length);
+
+    if (productList.result.length < this._pageSize) {
+      setState(() {
+        // this._productList = productList.result;
+        this._productList.addAll(productList.result);
+        this._hasMore = false;
+        this.flag = true;
+      });
+    } else {
+      setState(() {
+        // this._productList = productList.result;
+        this._productList.addAll(productList.result);
+        this._page++;
+        this.flag = true;
+      });
+    }
+  }
+
+  //显示加载中的圈圈
+  Widget _showMore(index) {
+    if (this._hasMore) {
+      return (index == this._productList.length - 1)
+          ? LoadingWidget()
+          : Text("");
+    } else {
+      return (index == this._productList.length - 1)
+          ? Text("--我是有底线的--")
+          : Text("");
+      ;
+    }
+  }
 
   //商品列表
   Widget _productListWidget() {
-    return Container(
-      padding: EdgeInsets.all(10),
-      margin: EdgeInsets.only(top: ScreenAdapter.height(80)), // 留下顶部横向导航的高度
-      child: ListView.builder(
-        itemBuilder: (context, index) {
-          //每一个元素
-          return Column(
-            children: <Widget>[
-              Row(
-                children: <Widget>[
-                  Container(
-                    width: ScreenAdapter.width(180),
-                    height: ScreenAdapter.height(180),
-                    child: Image.network(
-                        "https://www.itying.com/images/flutter/list2.jpg",
-                        fit: BoxFit.cover),
-                  ),
-                  Expanded(
-                    flex: 1,
-                    child: Container(
+    if (this._productList.length > 0) {
+      return Container(
+        padding: EdgeInsets.all(10),
+        margin: EdgeInsets.only(top: ScreenAdapter.height(80)),
+        child: ListView.builder(
+          controller: _scrollController,
+          itemBuilder: (context, index) {
+            //处理图片
+            String pic = this._productList[index].pic;
+            pic = Config.domain + pic.replaceAll('\\', '/');
+            //每一个元素
+            return Column(
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    Container(
+                      width: ScreenAdapter.width(180),
                       height: ScreenAdapter.height(180),
-                      margin: EdgeInsets.only(left: 10),
-                      // color: Colors.red,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text(
-                              "戴尔(DELL)灵越3670 英特尔酷睿i5 高性能 台式电脑整机(九代i5-9400 8G 256G)",
-                              maxLines: 2,
-                              overflow: TextOverflow.ellipsis),
-                          Row(
-                            children: <Widget>[
-                              Container(
-                                height: ScreenAdapter.height(36),
-                                margin: EdgeInsets.only(right: 10),
-                                padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
-
-                                //注意 如果Container里面加上decoration属性，这个时候color属性必须得放在BoxDecoration
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(10),
-                                  color: Color.fromRGBO(230, 230, 230, 0.9),
-                                ),
-
-                                child: Text("4g"),
-                              ),
-                              Container(
-                                height: ScreenAdapter.height(36),
-                                margin: EdgeInsets.only(right: 10),
-                                padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(10),
-                                  color: Color.fromRGBO(230, 230, 230, 0.9),
-                                ),
-                                child: Text("126"),
-                              ),
-                            ],
-                          ),
-                          Text(
-                            "¥990",
-                            style: TextStyle(color: Colors.red, fontSize: 16),
-                          )
-                        ],
-                      ),
+                      child: Image.network("${pic}", fit: BoxFit.cover),
                     ),
-                  )
-                ],
-              ),
-              Divider(height: 20)
-            ],
-          );
-        },
-        itemCount: 10,
-      ),
-    );
+                    Expanded(
+                      flex: 1,
+                      child: Container(
+                        height: ScreenAdapter.height(180),
+                        margin: EdgeInsets.only(left: 10),
+                        // color: Colors.red,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text("${this._productList[index].title}",
+                                maxLines: 2, overflow: TextOverflow.ellipsis),
+                            Row(
+                              children: <Widget>[
+                                Container(
+                                  height: ScreenAdapter.height(36),
+                                  margin: EdgeInsets.only(right: 10),
+                                  padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+
+                                  //注意 如果Container里面加上decoration属性，这个时候color属性必须得放在BoxDecoration
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(10),
+                                    color: Color.fromRGBO(230, 230, 230, 0.9),
+                                  ),
+
+                                  child: Text("4g"),
+                                ),
+                                Container(
+                                  height: ScreenAdapter.height(36),
+                                  margin: EdgeInsets.only(right: 10),
+                                  padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(10),
+                                    color: Color.fromRGBO(230, 230, 230, 0.9),
+                                  ),
+                                  child: Text("126"),
+                                ),
+                              ],
+                            ),
+                            Text(
+                              "¥${this._productList[index].price}",
+                              style: TextStyle(color: Colors.red, fontSize: 16),
+                            )
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+                Divider(height: 20),
+                _showMore(index)
+              ],
+            );
+          },
+          itemCount: this._productList.length,
+        ),
+      );
+    } else {
+      //加载中
+      return LoadingWidget();
+    }
   }
 
   //筛选导航
@@ -156,7 +252,6 @@ class _ProductListPageState extends State<ProductListPage> {
                   child: Text("筛选", textAlign: TextAlign.center),
                 ),
                 onTap: () {
-                  //注意：新版本中ScaffoldState? 为可空类型 注意判断
                   if (_scaffoldKey.currentState != null) {
                     _scaffoldKey.currentState!.openEndDrawer();
                   }
@@ -176,7 +271,7 @@ class _ProductListPageState extends State<ProductListPage> {
         appBar: AppBar(
           title: Text("商品列表"),
           leading: IconButton(
-            //注意：新版本的Flutter中加入Drawer组件会导致默认的返回按钮失效，所以需要手动加上返回按钮
+            //注意：新版本的Flutter中加入Drawer组件会导致默认的返回按钮失效，所以我们需要手动加一下返回按钮
             icon: Icon(Icons.arrow_back),
             onPressed: () {
               Navigator.pop(context);
@@ -192,7 +287,7 @@ class _ProductListPageState extends State<ProductListPage> {
         body: Stack(
           children: <Widget>[
             _productListWidget(),
-            _subHeaderWidget(), // 顶部横向导航，通过定位的方式布局
+            _subHeaderWidget(),
           ],
         ));
   }
