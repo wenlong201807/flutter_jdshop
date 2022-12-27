2ed6f67ec20a118b64e71f8916385c7721a27c79
dragon <1573511441@qq.com>
2022-12-27 10:54:06 +0800

    feat: 1、首页-推荐商品-竖向滑动列表 2、组件抽离

--- lib/pages/tabs/Home.dart
+++ lib/pages/tabs/Home.dart
@@ -54,23 +54,25 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-  Widget _hotProductListWidget(){
-    return  Container(
+  Widget _hotProductListWidget() {
+    return Container(
       height: ScreenAdapter.height(234), // 外层必须设置高度
       padding: EdgeInsets.all(ScreenAdapter.width(20)),
       child: ListView.builder(
         scrollDirection: Axis.horizontal, // 水平方向滑动列表
-        itemBuilder: (contxt,index){
+        itemBuilder: (contxt, index) {
           return Column(
             children: <Widget>[
               Container(
                 height: ScreenAdapter.height(140),
                 width: ScreenAdapter.width(140),
                 margin: EdgeInsets.only(right: ScreenAdapter.width(21)),
-                child: Image.network("https://www.itying.com/images/flutter/hot${index+1}.jpg",fit: BoxFit.cover),
+                child: Image.network(
+                    "https://www.itying.com/images/flutter/hot${index + 1}.jpg",
+                    fit: BoxFit.cover),
               ),
               Container(
-                padding: EdgeInsets.only(top:ScreenAdapter.height(10)),
+                padding: EdgeInsets.only(top: ScreenAdapter.height(10)),
                 height: ScreenAdapter.height(44),
                 child: Text("第${index + 1}条"),
               )
@@ -82,17 +84,91 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
+  //推荐商品
+  _recProductItemWidget() {
+    var itemWidth = (ScreenAdapter.getScreenWidth() - 30) / 2;
+
+    return Container(
+      padding: const EdgeInsets.all(10),
+      width: itemWidth, // 单个卡片宽度计算
+      decoration: BoxDecoration(
+          border:
+              Border.all(color: const Color.fromRGBO(233, 233, 233, 0.9), width: 1)),
+      child: Column(
+        children: <Widget>[
+          SizedBox(
+            width: double.infinity, // 与父级等宽
+            child: AspectRatio(
+              //防止服务器返回的图片大小不一致导致高度不一致问题
+              aspectRatio: 1 / 1,
+              child: Image.network(
+                "https://www.itying.com/images/flutter/list1.jpg",
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: ScreenAdapter.height(20)),
+            child: const Text(
+              "2019夏季新款气质高贵洋气阔太太有女人味中长款宽松大码",
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+              style: TextStyle(color: Colors.black54),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: ScreenAdapter.height(20)),
+            child: Stack(
+              children: const <Widget>[
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    "¥188.0",
+                    style: TextStyle(color: Colors.red, fontSize: 16),
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: Text("¥198.0",
+                      style: TextStyle(
+                          color: Colors.black54,
+                          fontSize: 14,
+                          decoration: TextDecoration.lineThrough)),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return ListView(
       children: <Widget>[
         _swiperWidget(),
-         SizedBox(height: ScreenAdapter.height(12)),
+        SizedBox(height: ScreenAdapter.height(20)),
         _titleWidget("猜你喜欢"),
         SizedBox(height: ScreenAdapter.height(34)),
         _hotProductListWidget(),
-        SizedBox(height: ScreenAdapter.height(12)),
+        SizedBox(height: ScreenAdapter.height(20)),
         _titleWidget("热门推荐"),
+        Container(
+          padding: const EdgeInsets.all(10),
+          child: Wrap(
+            runSpacing: 10,
+            spacing: 10,
+            children: <Widget>[
+              _recProductItemWidget(),
+              _recProductItemWidget(),
+              _recProductItemWidget(),
+              _recProductItemWidget(),
+              _recProductItemWidget(),
+              _recProductItemWidget()
+            ],
+          ),
+        )
       ],
     );
   }
