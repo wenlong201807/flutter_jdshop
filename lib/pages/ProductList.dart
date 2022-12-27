623d7d33ad2d04cdbf73587b70c2032ecd39e844
dragon <1573511441@qq.com>
2022-12-27 19:16:00 +0800

    feat: 1、商品列表页 - 筛选条件实现

--- lib/pages/ProductList.dart
+++ lib/pages/ProductList.dart
@@ -8,7 +8,6 @@ import '../widget/LoadingWidget.dart';
 
 class ProductListPage extends StatefulWidget {
   Map arguments;
-  //required this.arguments表示arguments必须传入值
   ProductListPage({Key? key, required this.arguments}) : super(key: key);
 
   _ProductListPageState createState() => _ProductListPageState();
@@ -18,33 +17,41 @@ class _ProductListPageState extends State<ProductListPage> {
   //Scaffold key
   final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
 
-  //用于上拉分页
-  ScrollController _scrollController = ScrollController(); //listview 的控制器
-
+  //用于上拉分页 listview 的控制器
+  ScrollController _scrollController = ScrollController();
   //分页
   int _page = 1;
-
   //每页有多少条数据
   int _pageSize = 8;
-
   //数据
   List _productList = [];
   /*
   排序:价格升序 sort=price_1 价格降序 sort=price_-1  销量升序 sort=salecount_1 销量降序 sort=salecount_-1
   */
   String _sort = "";
-
   //解决重复请求的问题
   bool flag = true;
-
   //是否有数据
   bool _hasMore = true;
-
+  /*二级导航数据*/
+  List _subHeaderList = [
+    {
+      "id": 1,
+      "title": "综合",
+      "fileds": "all",
+      "sort":
+          -1, //排序     升序：price_1     {price:1}        降序：price_-1   {price:-1}
+    },
+    {"id": 2, "title": "销量", "fileds": 'salecount', "sort": -1},
+    {"id": 3, "title": "价格", "fileds": 'price', "sort": -1},
+    {"id": 4, "title": "筛选"}
+  ];
+  //二级导航选中判断
+  int _selectHeaderId = 1;
   @override
   void initState() {
     super.initState();
     _getProductListData();
-
     //监听滚动条滚动事件
     _scrollController.addListener(() {
       //_scrollController.position.pixels //获取滚动条滚动的高度
@@ -63,27 +70,20 @@ class _ProductListPageState extends State<ProductListPage> {
     setState(() {
       this.flag = false;
     });
-
     var api =
         '${Config.domain}api/plist?cid=${widget.arguments["cid"]}&page=${this._page}&sort=${this._sort}&pageSize=${this._pageSize}';
 
     print(api);
     var result = await Dio().get(api);
-
     var productList = new ProductModel.fromJson(result.data);
-
-    print(productList.result.length);
-
     if (productList.result.length < this._pageSize) {
       setState(() {
-        // this._productList = productList.result;
         this._productList.addAll(productList.result);
         this._hasMore = false;
         this.flag = true;
       });
     } else {
       setState(() {
-        // this._productList = productList.result;
         this._productList.addAll(productList.result);
         this._page++;
         this.flag = true;
@@ -102,6 +102,36 @@ class _ProductListPageState extends State<ProductListPage> {
           ? Text("--我是有底线的--")
           : Text("");
       ;
+    }
+  }
+
+//导航改变的时候触发
+  _subHeaderChange(id) {
+    if (id == 4) {
+      _scaffoldKey.currentState!.openEndDrawer();
+      setState(() {
+        this._selectHeaderId = id;
+      });
+    } else {
+      setState(() {
+        this._selectHeaderId = id;
+        this._sort =
+            "${this._subHeaderList[id - 1]["fileds"]}_${this._subHeaderList[id - 1]["sort"]}";
+
+        //重置分页
+        this._page = 1;
+        //重置数据
+        this._productList = [];
+        //改变sort排序
+        this._subHeaderList[id - 1]['sort'] =
+            this._subHeaderList[id - 1]['sort'] * -1;
+        //回到顶部
+        _scrollController.jumpTo(0);
+        //重置_hasMore
+        this._hasMore = true;
+        //重新请求
+        this._getProductListData();
+      });
     }
   }
 
@@ -190,6 +220,17 @@ class _ProductListPageState extends State<ProductListPage> {
     }
   }
 
+  //显示header Icon
+  Widget _showIcon(id) {
+    if (id == 2 || id == 3) {
+      if (this._subHeaderList[id - 1]["sort"] == 1) {
+        return Icon(Icons.arrow_drop_down);
+      }
+      return Icon(Icons.arrow_drop_up);
+    }
+    return Text("");
+  }
+
   //筛选导航
   Widget _subHeaderWidget() {
     return Positioned(
@@ -205,60 +246,34 @@ class _ProductListPageState extends State<ProductListPage> {
                 bottom: BorderSide(
                     width: 1, color: Color.fromRGBO(233, 233, 233, 0.9)))),
         child: Row(
-          children: <Widget>[
-            Expanded(
+          children: this._subHeaderList.map((value) {
+            return Expanded(
               flex: 1,
               child: InkWell(
                 child: Padding(
                   padding: EdgeInsets.fromLTRB(
                       0, ScreenAdapter.height(16), 0, ScreenAdapter.height(16)),
-                  child: Text(
-                    "综合",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(color: Colors.red),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        "${value["title"]}",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            color: (this._selectHeaderId == value["id"])
+                                ? Colors.red
+                                : Colors.black54),
+                      ),
+                      _showIcon(value["id"])
+                    ],
                   ),
                 ),
-                onTap: () {},
-              ),
-            ),
-            Expanded(
-              flex: 1,
-              child: InkWell(
-                child: Padding(
-                  padding: EdgeInsets.fromLTRB(
-                      0, ScreenAdapter.height(16), 0, ScreenAdapter.height(16)),
-                  child: Text("销量", textAlign: TextAlign.center),
-                ),
-                onTap: () {},
-              ),
-            ),
-            Expanded(
-              flex: 1,
-              child: InkWell(
-                child: Padding(
-                  padding: EdgeInsets.fromLTRB(
-                      0, ScreenAdapter.height(16), 0, ScreenAdapter.height(16)),
-                  child: Text("价格", textAlign: TextAlign.center),
-                ),
-                onTap: () {},
-              ),
-            ),
-            Expanded(
-              flex: 1,
-              child: InkWell(
-                child: Padding(
-                  padding: EdgeInsets.fromLTRB(
-                      0, ScreenAdapter.height(16), 0, ScreenAdapter.height(16)),
-                  child: Text("筛选", textAlign: TextAlign.center),
-                ),
                 onTap: () {
-                  if (_scaffoldKey.currentState != null) {
-                    _scaffoldKey.currentState!.openEndDrawer();
-                  }
+                  _subHeaderChange(value["id"]);
                 },
               ),
-            ),
-          ],
+            );
+          }).toList(),
         ),
       ),
     );
@@ -271,12 +286,11 @@ class _ProductListPageState extends State<ProductListPage> {
         appBar: AppBar(
           title: Text("商品列表"),
           leading: IconButton(
-            //注意：新版本的Flutter中加入Drawer组件会导致默认的返回按钮失效，所以我们需要手动加一下返回按钮
-            icon: Icon(Icons.arrow_back),
-            onPressed: () {
-              Navigator.pop(context);
-            },
-          ),
+              onPressed: () {
+                //注意：新版本的Flutter中加入Drawer组件会导致默认的返回按钮失效，所以我们需要手动加一下返回按钮
+                Navigator.pop(context);
+              },
+              icon: Icon(Icons.arrow_back)),
           actions: <Widget>[Text("")],
         ),
         endDrawer: Drawer(
