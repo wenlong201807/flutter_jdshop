b0c1be7bdee2ec845bbc89a0d89d32c5d3d1a169
dragon <1573511441@qq.com>
2022-12-27 10:39:47 +0800

    feat: 1、首页-猜你喜欢-横向滑动列表 2、组件抽离

--- lib/pages/tabs/Home.dart
+++ lib/pages/tabs/Home.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 //最新版本的flutter中使用flutter_swiper_null_safety替代flutter_swiper，他们的用法都是一样的。
 import 'package:flutter_swiper_null_safety/flutter_swiper_null_safety.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
+import '../../services/ScreenAdapter.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -37,19 +37,47 @@ class _HomePageState extends State<HomePage> {
 
   Widget _titleWidget(value) {
     return Container(
-      height: ScreenUtil().setHeight(34),
-      margin: EdgeInsets.only(left: ScreenUtil().setWidth(20)),
-      padding: EdgeInsets.only(left: ScreenUtil().setWidth(20)),
+      height: ScreenAdapter.height(34),
+      margin: EdgeInsets.only(left: ScreenAdapter.width(20)),
+      padding: EdgeInsets.only(left: ScreenAdapter.width(20)),
       decoration: BoxDecoration(
           border: Border(
               left: BorderSide(
         color: Colors.red,
-        width: ScreenUtil().setWidth(10),
+        width: ScreenAdapter.width(10),
       ))),
       child: Text(
         value,
         style:
-            TextStyle(color: Colors.black54, fontSize: ScreenUtil().setSp(26)),
+            TextStyle(color: Colors.black54, fontSize: ScreenAdapter.size(26)),
+      ),
+    );
+  }
+
+  Widget _hotProductListWidget(){
+    return  Container(
+      height: ScreenAdapter.height(234), // 外层必须设置高度
+      padding: EdgeInsets.all(ScreenAdapter.width(20)),
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal, // 水平方向滑动列表
+        itemBuilder: (contxt,index){
+          return Column(
+            children: <Widget>[
+              Container(
+                height: ScreenAdapter.height(140),
+                width: ScreenAdapter.width(140),
+                margin: EdgeInsets.only(right: ScreenAdapter.width(21)),
+                child: Image.network("https://www.itying.com/images/flutter/hot${index+1}.jpg",fit: BoxFit.cover),
+              ),
+              Container(
+                padding: EdgeInsets.only(top:ScreenAdapter.height(10)),
+                height: ScreenAdapter.height(44),
+                child: Text("第${index + 1}条"),
+              )
+            ],
+          );
+        },
+        itemCount: 10,
       ),
     );
   }
@@ -59,9 +87,11 @@ class _HomePageState extends State<HomePage> {
     return ListView(
       children: <Widget>[
         _swiperWidget(),
-        const SizedBox(height: 10),
+         SizedBox(height: ScreenAdapter.height(12)),
         _titleWidget("猜你喜欢"),
-        const SizedBox(height: 10),
+        SizedBox(height: ScreenAdapter.height(34)),
+        _hotProductListWidget(),
+        SizedBox(height: ScreenAdapter.height(12)),
         _titleWidget("热门推荐"),
       ],
     );
