3897603430c6e7efb5efbdd560541cf4b2285087
dragon <1573511441@qq.com>
2022-12-28 08:28:11 +0800

    feat: 商品详情页面-商品tab： 1、页面静态布局 2、底部定位按钮

--- lib/pages/ProductContent.dart
+++ lib/pages/ProductContent.dart
@@ -1,25 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import '../services/ScreenAdapter.dart';
 
 import 'ProductContent/ProductContentFirst.dart';
 import 'ProductContent/ProductContentSecond.dart';
 import 'ProductContent/ProductContentThird.dart';
+import '../widget/JdButton.dart';
 
 class ProductContentPage extends StatefulWidget {
   final Map arguments;
   ProductContentPage({Key? key, required this.arguments}) : super(key: key);
+
   _ProductContentPageState createState() => _ProductContentPageState();
 }
 
 class _ProductContentPageState extends State<ProductContentPage> {
-
-  @override
-  void initState() {
-    super.initState();
-
-    print(widget.arguments['id']);
-  }
-
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
@@ -49,32 +44,43 @@ class _ProductContentPageState extends State<ProductContentPage> {
               )
             ],
           ),
+
           actions: <Widget>[
+
             IconButton(
               icon: Icon(Icons.more_horiz),
-              onPressed: () {
+              onPressed: (){
                 showMenu(
-                    context: context,
-                    position: RelativeRect.fromLTRB(
-                        ScreenAdapter.width(600), 76, 10, 0),
+                    context:context,
+                    position:RelativeRect.fromLTRB(ScreenAdapter.width(600), 76, 10, 0) ,
                     items: [
                       PopupMenuItem(
                         child: Row(
-                          children: <Widget>[Icon(Icons.home), Text("首页")],
+                          children: <Widget>[
+                            Icon(Icons.home),
+                            Text("首页")
+                          ],
                         ),
                       ),
                       PopupMenuItem(
                         child: Row(
-                          children: <Widget>[Icon(Icons.search), Text("搜索")],
+                          children: <Widget>[
+                            Icon(Icons.search),
+                            Text("搜索")
+                          ],
                         ),
                       )
-                    ]);
+                    ]
+                );
               },
             )
+
           ],
         ),
         body: Stack(
+
           children: <Widget>[
+
             TabBarView(
               children: <Widget>[
                 ProductContentFirst(),
@@ -84,11 +90,55 @@ class _ProductContentPageState extends State<ProductContentPage> {
             ),
             Positioned(
               width: ScreenAdapter.width(750),
-              height: ScreenAdapter.width(80),
+              height: ScreenAdapter.width(88), //注意：里面元素的总高度不能大于外部元素的高度
               bottom: 0,
               child: Container(
-                color: Colors.red,
-                child: Text("底部"),
+                decoration: BoxDecoration(
+                    border: Border(
+                        top: BorderSide(
+                            color: Colors.black26,
+                            width: 1
+                        )
+                    ),
+                    color: Colors.white
+                ),
+                child: Row(
+                  children: <Widget>[
+
+                    Container(
+                      padding: EdgeInsets.only(top:ScreenAdapter.height(8)),
+                      width: 100,
+                      height: ScreenAdapter.height(80),
+                      child: Column(
+                        children: <Widget>[
+                          Icon(Icons.shopping_cart,size:ScreenAdapter.size(38)),
+                          Text("购物车",style: TextStyle(fontSize:ScreenAdapter.size(24)))
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      flex: 1,
+                      child: JdButton(
+                        color:Color.fromRGBO(253, 1, 0, 0.9),
+                        text: "加入购物车",
+                        cb: (){
+                          print('加入购物车');
+                        },
+                      ),
+                    ),
+                    Expanded(
+                      flex: 1,
+                      child: JdButton(
+                        color: Color.fromRGBO(255, 165, 0, 0.9),
+                        text: "立即购买",
+                        cb: (){
+                          print('立即购买');
+                        },
+                      ),
+                    )
+
+                  ],
+                ),
               ),
             )
           ],
