018ccee5cd7e0c61588822d3fc0c706331102da2
dragon <1573511441@qq.com>
2022-12-28 12:22:13 +0800

    feat: 购物车页面 1、provider状态管理数据， 2、用户中心共享数据

--- lib/main.dart
+++ lib/main.dart
@@ -2,33 +2,40 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'routers/router.dart';
 
-void main() => runApp(const MyApp());
+// 引入provider
+import 'package:provider/provider.dart';
+import 'provider/Counter.dart';
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  @override
   _MyAppState createState() => _MyAppState();
 }
-
 
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    //Set the fit size (Find your UI design, look at the dimensions of the device screen and fill it in,unit in dp)
     return ScreenUtilInit(
       designSize: const Size(750, 1334), //配置设计稿的宽度高度
       minTextAdapt: true,
       splitScreenMode: true,
       builder: (context, child) {
-        return MaterialApp(
-          debugShowCheckedModeBanner: false,
-          initialRoute: '/',
-          onGenerateRoute: onGenerateRoute,
-          theme: ThemeData(
-            primaryColor: Colors.white,
+        return MultiProvider(
+          providers: [
+            ChangeNotifierProvider(create: (_) => Counter()),
+          ],
+          child: MaterialApp(
+            debugShowCheckedModeBanner: false,
+            initialRoute: '/',
+            onGenerateRoute: onGenerateRoute,
+            theme: ThemeData(
+              primaryColor: Colors.white,
+            ),
           ),
         );
+
       },
     );
   }
--- lib/pages/tabs/Cart.dart
+++ lib/pages/tabs/Cart.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../../provider/Counter.dart';
 
 class CartPage extends StatefulWidget {
   CartPage({Key? key}) : super(key: key);
@@ -8,7 +10,29 @@ class CartPage extends StatefulWidget {
 
 class _CartPageState extends State<CartPage> {
   @override
+  void initState() {
+    super.initState();
+    print("cart");
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Text("购物车页面");
+
+    // print('1111111111111----');
+    var counterProvider = Provider.of<Counter>(context);
+
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          counterProvider.incCount();
+        },
+        child: Icon(Icons.add),
+      ),
+      body: Center(
+        child: Text("${counterProvider.count}",style: TextStyle(
+            fontSize: 50
+        )),
+      ),
+    );
   }
 }
